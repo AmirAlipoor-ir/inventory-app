df55948d2dece57a25c102f419642cf382c4ae29
AmirAlipoor-ir <89448149+AmirAlipoor-ir@users.noreply.github.com>
2023-11-23 09:05:53 +0330

    add NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="bg-slate-800 min-h-screen">
+      <div className="h-12 flex items-center justify-center gap-x-4 bg-slate-700 mb-6">
+        <h1 className="md:text-xl text-sm font-bold text-slate-300">
+          Inventory App
+        </h1>
+        <span className="flex items-center justify-center w-7 h-7 rounded-full bg-slate-500 border-2 border-slate-300">
+          7
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
+ 
