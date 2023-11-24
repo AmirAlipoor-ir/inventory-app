b87274b8d34c42cd5b2ef2beed23bc6927888191
AmirAlipoor-ir <89448149+AmirAlipoor-ir@users.noreply.github.com>
2023-11-24 19:04:30 +0330

    add tailwindcss Forms plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
+import Category from "./components/Category";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <div className="bg-slate-800 min-h-screen">
         <NavBar />
-        <div className="container max-w-screen-sm ms-auto p-4"></div>
+        <div className="container max-w-screen-sm mx-auto p-4">
+          <Category />
+        </div>
       </div>
     </div>
   );
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React from "react";
 
 function Category() {
   return (
     <section>
-      <div></div>
+      <div className="mb-6" id="category-wrapper">
+        <h2 className="text-xl text-slate-300 font-bold mb-2">
+          Add New category
+        </h2>
+        <form className="bg-slate-700 p-4 rounded-xl flex flex-col gap-y-4">
+          <div>
+            <label for="category-title" className="block mb-1 text-slate-400">
+              title
+            </label>
+            <input
+              type="text"
+              name="category-title"
+              id="category-title"
+              className="bg-transparent rounded-xl border border-slate-500 text-slate-400 w-full md:w-auto"
+            />
+          </div>
+          <div>
+            <label
+              for="category-description"
+              className="block mb-1 text-slate-400"
+            >
+              description
+            </label>
+            <textarea
+              className="bg-transparent rounded-xl border border-slate-500 text-slate-400 w-full"
+              type=" text"
+              name="category-description"
+              id="category-description"
+            ></textarea>
+          </div>
+          <div className="flex items-center justify-between gap-x-4">
+            <button
+              className="flex-1 border border-slate-400 text-slate-400 rounded-xl py-2"
+              id="cancel-add-category"
+            >
+              Cancel
+            </button>
+            <button
+              id="add-new-category"
+              className="flex-1 bg-slate-500 text-slate-200 rounded-xl py-2"
+            >
+              Add Category
+            </button>
+          </div>
+        </form>
+      </div>
+      <button
+        id="toggle-add-category"
+        className="text-slate-600 text-lg mb-4 font-medium"
+      >
+        Add new Category?
+      </button>
     </section>
-  )
+  );
 }
 
-export default Category
+export default Category;
